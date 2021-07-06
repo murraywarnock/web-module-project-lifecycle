27410b8fe03cc6b76aed4b4a6048147aa28cc936
MurrayWarnock <murray.warnock@gmail.com>
2021-07-06 16:38:24 -0600

    Cleanup; refactored UserCard component to eliminate Follower.

--- lc-methods/src/UserCard.js
+++ lc-methods/src/UserCard.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import Follower from './Follower';
 
 class UserCard extends React.Component {
-
-  componentDidMount() {
-  }  
-
-  componentDidUpdate() {
-    // console.log("UserCard: Component Updates");
-  }
     
   render() {
       return(
         <>
-        {this.props.followers.map(follower => (
-          <div key={follower.name}>
-            <Follower follower={follower} seeFollowers={this.props.seeFollowers}/>
+        {this.props.followers.map((follower) => {
+          return(
+        <div key={follower.login} className="follower">
+          <div className="follower-content"><img src={`${follower.avatar_url}`} alt=""/>
+            <p>{follower.login}</p>
+            <div className="see-followers" onClick={()=>{this.props.seeFollowers(follower.login)}}>
+              SEE FOLLOWERS
+            </div>
           </div>
-        ))}
+        </div>)})
+      }
       </>
-      );
+    );
   }
 }
 
